55ebf5de39872eb73217331df860f071a2e054ed
puneeeeth <chitikeshipuneeth001@gmail.com>
2023-08-18 18:53:00 +0530

    new changes to main.tf file

--- lb+vms/main.tf
+++ lb+vms/main.tf
@@ -1,15 +1,15 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "3.69.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.69.0"
+    }
+  }
+}
 
-# provider "azurerm" {
-#   features { }
-# }
+provider "azurerm" {
+  features { }
+}
 
 resource "azurerm_resource_group" "newrg" {
   name     = "newRG01"
