79f9d275527110da9b31c21862b997a8045d4e8a
puneeeeth <95013956+puneeeeth@users.noreply.github.com>
2023-08-19 12:51:00 +0530

    Update main.tf

--- lb+vms/main.tf
+++ lb+vms/main.tf
@@ -1,15 +1,12 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.69.0"
-    }
+  backend "azurerm" {
+    /*resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"*/
   }
 }
 
-provider "azurerm" {
-  features { }
-}
 
 resource "azurerm_resource_group" "newrg" {
   name     = "newRG01"
